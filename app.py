5a10bccabc8173aa57caccb6f2faf88b5c619232
lolek0102 <101627932+lolek0102@users.noreply.github.com>
2025-01-05 15:56:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    bmi_result = None
-    if request.method == 'POST':
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        birth_date = request.form.get('birth_date')
-        email = request.form.get('email')
-        height = request.form.get('height')
-        weight = request.form.get('weight')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        if '@' not in email:
-            return render_template('form.html', error="Nieprawidłowy adres email!")
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    height = float(request.form['height'])
+    weight = float(request.form['weight'])
 
-        try:
-            height = float(height)
-            weight = float(weight)
-            bmi_result = round(weight / (height / 100) ** 2, 2)
-        except ValueError:
-            return render_template('form.html', error="Wzrost i waga muszą być liczbami!")
+    bmi = weight / (height / 100) ** 2
+    bmi_category = ''
+    if bmi < 18.5:
+        bmi_category = 'Niedowaga'
+    elif 18.5 <= bmi < 24.9:
+        bmi_category = 'W normie'
+    elif 25 <= bmi < 29.9:
+        bmi_category = 'Nadwaga'
+    else:
+        bmi_category = 'Otyłość'
 
-    return render_template('form.html', bmi_result=bmi_result)
+    return render_template('result.html', first_name=first_name, last_name=last_name, bmi=bmi, bmi_category=bmi_category)
 
 if __name__ == '__main__':
     app.run(debug=True)
